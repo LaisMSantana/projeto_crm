f6861397af014d88b7067aeced96194014605db9
Lais <lais.santana123@hotmail.com>
2020-03-08 17:23:15 -0300

    Alteração do submit e validações na tela

--- projetocrmangular/projetocrm/src/app/app.component.ts
+++ projetocrmangular/projetocrm/src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { Cliente } from './cliente';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs'
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { error } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -11,17 +14,35 @@ import { Observable } from 'rxjs'
 export class AppComponent {
 
   clienteModel = new Cliente("","","","");
+  submitted = false;
+  errorMsg = '';
+  succsessMsg = ''
+  hideSuccessMessage = false;
 
   url = 'http://localhost:8080/api/clientes';
 
   constructor(private http: HttpClient){}
 
 
-  onSubmit(){
-    this.http.post(this.url, this.clienteModel).toPromise().then((data:any) => {
-      console.log(this.clienteModel);
-    });
+  onSubmit(cliente){
+    this.submitted = true;
+    this.http.post<any>(this.url,cliente).pipe(catchError(this.errorHandler)).subscribe(
+      data => this.succsessMsg = "Cliente salvo com sucesso!",
+      error => this.errorMsg = error.statusText
+    )
+    this.hideSuccessMessage = false;
   }
+
+  errorHandler(error: HttpErrorResponse){
+    return throwError(error);
+  }
+
+  FadeOutSuccessMsg() {
+     setTimeout( () => {
+           this.hideSuccessMessage = true;
+        }, 3000);
+  }
+
 }
 
 
